a126ee4b73932dcdda18ff7b15c0e21c81548ec4
963559210@qq.com <963559210@qq.com>
2018-04-07 09:44:16 +0800

    切面编程，依赖注入完毕了

--- src/com/aiprogram/serveice/UserServiceImpl.java
+++ src/com/aiprogram/serveice/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.aiprogram.serveice;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.InvocationHandler;
+
 /**
  * Created by 平凡的世界 on 2018/4/3.
  */
